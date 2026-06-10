699ba442332c6c58c42c9954fc1ce431283825c5
Appletea <dongs4739@gmail.com>
2026-06-10 10:38:04 +0900

    Job04, Job05, ui

--- job04_recommendation.py
+++ job04_recommendation.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread
 import pickle
+
 
 
 def getRecommendation(cosine_sim):
@@ -19,7 +21,9 @@ Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pkl', 'rb') as f:
     Tfidf = pickle.load(f)
 
-ref_idx = 16
+# 영화 인덱스
+
+ref_idx = 1228
 print('title:', df_reviews.iloc[ref_idx]['titles'])
 
 cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
@@ -28,3 +32,26 @@ print(len(cosine_sim[0]))
 
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
+
+# key word 이용
+
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '사랑'
+if keyword not in list(embedding_model.wv.index_to_key):
+    print('모르는 단어입니다.')
+else:
+    sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+    print(sim_word)
+    sentence = [keyword] * 11
+    count = 10
+    for word, _ in sim_word:
+        sentence = sentence + [word] * count
+        count = count -1
+    print(sentence)
+    sentence = ' '.join(sentence)
+    print(sentence)
+
+    sentence_vec = Tfidf.transform([sentence])
+    cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+    recommendation = getRecommendation(cosine_sim)
+    print(recommendation)
